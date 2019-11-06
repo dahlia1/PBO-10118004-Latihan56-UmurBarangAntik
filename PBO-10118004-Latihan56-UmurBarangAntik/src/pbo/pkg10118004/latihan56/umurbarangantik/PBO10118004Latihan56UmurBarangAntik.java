2f4cdce00cc5e52813111634e4cb892224789a4d
dahlia1 <55682672+dahlia1@users.noreply.github.com>
2019-11-06 18:47:03 +0700

    Add files via upload

---
+++ PBO-10118004-Latihan56-UmurBarangAntik/src/pbo/pkg10118004/latihan56/umurbarangantik/PBO10118004Latihan56UmurBarangAntik.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10118004.latihan56.umurbarangantik;
+
+/**
+ *
+ * @author ASUS A411UF
+ */
+public class PBO10118004Latihan56UmurBarangAntik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Radio radio = new Radio(234);
+        radio.setName("Radio");
+        System.out.println("Nama barang Antik : "+radio.getName());
+        radio.tampilUmur();
+        }
+    
+}
